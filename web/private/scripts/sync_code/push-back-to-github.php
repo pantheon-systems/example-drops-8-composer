59eb4955f265214d9f7003ad69076e9d4ce8c5ee
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-01-06 14:27:43 -0800

    New version of push-back-to-github script.

--- web/private/scripts/sync_code/push-back-to-github.php
+++ web/private/scripts/sync_code/push-back-to-github.php
@@ -3,76 +3,63 @@
 /**
  * This script will attempt to push "lean" changes back upstream.
  */
-$repositoryRoot = dirname(dirname(dirname(__DIR__)));
-$bindingDir = dirname($repositoryRoot); // or $_SERVER['HOME']
+$bindingDir = $_SERVER['HOME'];
+$repositoryRoot = "$bindingDir/code";
+$docRoot = "$repositoryRoot/" . $_SERVER['DOCROOT'];
 
-include_once dirname(__DIR__) . 'lean-repo-utils.php';
+print "Enter push-back-to-github. repository root is $repositoryRoot, docRoot is $docRoot\n";
 
 $buildMetadataFile = "$repositoryRoot/build-metadata.json";
 if (!file_exists($buildMetadataFile)) {
+  print "Could not find build metadata file, $buildMetadataFile\n";
   return;
 }
 $buildMetadataFileContents = file_get_contents($buildMetadataFile);
-$buildMetadata = json_decode($buildMetadataFileContents);
+$buildMetadata = json_decode($buildMetadataFileContents, true);
 
 print "::::::::::::::::: Build Metadata :::::::::::::::::\n";
 var_export($buildMetadata);
 print "\n\n";
 
-/*
+$privateFiles = "$bindingDir/files/private";
+$gitHubSecretsFile = "$privateFiles/github-secrets.json";
+$gitHubSecretsContents = file_get_contents($gitHubSecretsFile);
+$gitHubSecrets = json_decode($gitHubSecretsContents, true);
 
-Old version
+print "::::::::::::::::: GitHub Secrets :::::::::::::::::\n";
+var_export($gitHubSecrets);
+print "\n\n";
 
-if (!in_array($_ENV['PANTHEON_ENVIRONMENT'], array('test', 'live'))) {
-  // Examine incoming commit to see if anything should go upstream.
-  $lean_files = array();
-  $non_lean_files = array();
-  $SHA = trim(`git rev-parse HEAD 2>&1`);
-  // Get all edited files in the commit we just recievd in sync_code.
-  echo "Looking for files to push upstream...\n";
-  #echo "git diff --name-only HEAD HEAD~1";
-  #echo `cd $workspace && git diff HEAD HEAD~1`;
-  #echo `git diff --name-only HEAD HEAD~1`;
-  #echo `git rev-parse HEAD~1`;
-  exec('git diff --name-only HEAD HEAD~1 2>&1', $output, $status);
-  foreach ($output as $file) {
-    if (`git cat-file _lean_upstream:$file -e` === NULL) {
-      $lean_files[] = $file;
-    }
-    else {
-      $non_lean_files[] = $file;
-    }
-  }
-  if (count($lean_files) > 0 && count($non_lean_files) > 0) {
-    // We crossed the streams.
-    pantheon_raise_dashboard_error('Mixed commit fail!');
-  }
-  if (count($lean_files) > 0 && count($non_lean_files) == 0) {
-    // Push the most recent hash.
-    echo "Last commit was just files tracked upstream:\n\n";
-    echo implode("\n", $lean_files);
-    // The process below does not work; `git push` will take all of
-    // the commits leading up to the one being moved, so this will make
-    // the lean repository fat.  We do not want that!
-    // We will need to use `git cherry-pick` instead.  The challenge
-    // here is that this command allows us to take a commit from a
-    // different branch, and apply it to the current branch.  We need
-    // to take commits from the current branch and apply them to some
-    // other branch.  Ideally, we do not want to switch branches in
-    // order to do this.
-    /*
-    echo "\n\nPushing...\n";
-    echo "\ncd $workspace && git push . $SHA:_lean_upstream 2>&1\n";
-    echo `cd $workspace && git push . $SHA:_lean_upstream 2>&1`;
-    echo "\ngit push $remote_url _lean_upstream:master 2>&1\n";
-    echo `git push $remote_url _lean_upstream:master 2>&1`;
-    */
-  }
-  else {
-    echo "No commits to push back upstream. All is well.";
-  }
-  // TODO, handle other branches
-  // echo `git symbolic-ref -q HEAD`;
-}
+// The remote repo to push to
+$upstreamRepo = $gitHubSecrets['repo']['url'];
 
-*/
+// The last commit made on the lean repo prior to creating the build artifacts
+$fromSha = $buildMetadata['sha'];
+
+// The name of the PR branch
+$branch = $buildMetadata['ref'];
+
+// The name of our new branch
+$prBranch = "new-$branch";
+
+// The commit to cherry-pick
+$currentCommit = exec('git rev-parse HEAD');
+
+// TODO: vet the contents of the commit for applicability first.
+// If the commit is 'mixed', it must be rejected. Is there any
+// way to recover from this? Maybe not.
+
+print "::::::::::::::::: Info :::::::::::::::::\n";
+print "We are going to check out $prBranch from $fromSha, then cherry-pick $currentCommit and push it back to $upstreamRepo\n";
+
+// Create our new branch
+passthru("git checkout -B $prBranch $fromSha");
+
+// Cherry-pick the commit just made
+passthru("git cherry-pick -Xthiers $currentCommit");
+
+// Go back to the branch we were on before
+passthru('git checkout -');
+
+// Push the new branch back to Pantheon
+passthru("git push $upstreamRepo $prBranch");
