efa149aba2e89a7ca40ccbaf58079fbb50be33cb
Steve Persch <steve.persch@pantheon.io>
2018-01-09 19:50:46 -0600

    set contenta sync dir

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -27,10 +27,13 @@ $config_directories = array(
 // sync dir accordingly. @todo, I hope to think of a better option.
 // Contenta install will fail if the sync dir is set to `/config`. But that is
 // what the sync dir should be during normal usage.
-$drush_args = drush_get_arguments();
-if (!empty($drush_args[0])  && 'site-install' === $drush_args[0]) {
+if (function_exists('drush_get_arguments')) {
+  $drush_args = drush_get_arguments();
+  if (!empty($drush_args[0])  && 'site-install' === $drush_args[0]) {
     $config_directories['sync'] = 'profiles/contrib/contenta_jsonapi/config/sync';
+  }
 }
+
 
 /**
  * If there is a local settings file, then include it
