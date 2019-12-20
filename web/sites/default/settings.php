a1d1cfbc76f7a08f0105786d76b11ad36d190490
Alison Jo <2660348+alisonjo315@users.noreply.github.com>
2019-12-20 13:57:27 -0500

    Update settings.php to latest Drupal 8.8.0 recommendations.
    Use $settings['config_sync_directory'] instead of $config_directories[CONFIG_SYNC_DIRECTORY].

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -19,9 +19,7 @@ include __DIR__ . "/settings.pantheon.php";
 /**
  * Place the config directory outside of the Drupal root.
  */
-$config_directories = array(
-  CONFIG_SYNC_DIRECTORY => dirname(DRUPAL_ROOT) . '/config',
-);
+$settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config';
 
 /**
  * If there is a local settings file, then include it
