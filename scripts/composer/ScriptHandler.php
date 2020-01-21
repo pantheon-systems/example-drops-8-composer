9d881980b30522c1a29e010b6c9b9b2213433ae0
Greg Anderson <greg@pantheon.io>
2020-01-21 13:43:06 -0800

    Try integrated composer with Build Tools.

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -47,37 +47,4 @@ class ScriptHandler
     }
   }
 
-  // This is called by the QuickSilver deploy hook to convert from
-  // a 'lean' repository to a 'fat' repository. This should only be
-  // called when using this repository as a custom upstream, and
-  // updating it with `terminus composer <site>.<env> update`. This
-  // is not used in the GitHub PR workflow.
-  public static function prepareForPantheon()
-  {
-    // Get rid of any .git directories that Composer may have added.
-    // n.b. Ideally, there are none of these, as removing them may
-    // impair Composer's ability to update them later. However, leaving
-    // them in place prevents us from pushing to Pantheon.
-    $dirsToDelete = [];
-    $finder = new Finder();
-    foreach (
-      $finder
-        ->directories()
-        ->in(getcwd())
-        ->ignoreDotFiles(false)
-        ->ignoreVCS(false)
-        ->depth('> 0')
-        ->name('.git')
-      as $dir) {
-      $dirsToDelete[] = $dir;
-    }
-    $fs = new Filesystem();
-    $fs->remove($dirsToDelete);
-
-    // Fix up .gitignore: remove everything above the "::: cut :::" line
-    $gitignoreFile = getcwd() . '/.gitignore';
-    $gitignoreContents = file_get_contents($gitignoreFile);
-    $gitignoreContents = preg_replace('/.*::: cut :::*/s', '', $gitignoreContents);
-    file_put_contents($gitignoreFile, $gitignoreContents);
-  }
 }
